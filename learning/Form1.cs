d3af959d99ba6535084487e905f6b55de84e7d42
hugofonseca <hugo.mig.28@gmail.com>
2016-06-10 10:23:55 -0300

    FirstCommitofIterativoxRecursivo
    O hugo é um bosta, o ronald é muito lindo

---
+++ learning/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace learning
+{
+    public partial class Form1 : Form
+    {
+        int[] oi = new int[5] { 1, 2, 3, 4, 5 };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine(Oi(oi));
+        }
+        public int Oi(int[] valores)
+        {
+            int total = 0;
+            foreach (int i in valores)
+            {
+                total += i;
+            }
+            return total;
+        }
+    }
+}
